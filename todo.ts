51d95de149b98138e47c0184dec38892e3bf62fa
jvstln <173433315+jvstln@users.noreply.github.com>
2025-02-27 05:11:20 +0100

    feat: create TodoList class with necessary methods

---
+++ todo.ts
@@ -0,0 +1,71 @@
+interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+class TodoList {
+  private todos: TodoItem[] = [];
+
+  // A helper method to get the todo item by id
+  getTodo(id: number): TodoItem | false {
+    const selectedTodo = this.todos.find((todo) => todo.id === id);
+    if (!selectedTodo) {
+      console.log("Todo not found");
+      return false;
+    }
+    return selectedTodo;
+  }
+
+  addTodo(task: string): void {
+    const todo: TodoItem = {
+      id: Math.random(), // Generate a random number as the id
+      task,
+      completed: false,
+    };
+    this.todos.push(todo);
+    console.log("Todo added");
+  }
+
+  completeTodo(id: number): void {
+    const selectedTodo = this.getTodo(id);
+    if (selectedTodo) {
+      selectedTodo.completed = true;
+      console.log("Todo completed");
+    }
+  }
+
+  removeTodo(id: number): void {
+    const selectedTodo = this.getTodo(id);
+    if (selectedTodo) {
+      this.todos.splice(this.todos.indexOf(selectedTodo), 1);
+      console.log("Todo removed");
+    }
+  }
+
+  listTodos(): TodoItem[] {
+    console.log("- List of todos -");
+    this.todos.forEach((todo) => {
+      console.log(`Task: ${todo.task}, Completed: ${todo.completed}`);
+    });
+    console.log("------------------");
+
+    return this.todos;
+  }
+
+  getTodosByStatus(completed: boolean): TodoItem[] {
+    return this.todos.filter((todo) => todo.completed === completed);
+  }
+
+  updateTodoTask(id: number, task: string): void {
+    const selectedTodo = this.getTodo(id);
+    if (selectedTodo) {
+      selectedTodo.task = task;
+      console.log("Todo task updated");
+    }
+  }
+}
+
+const todo = new TodoList();
+
+todo.listTodos();
